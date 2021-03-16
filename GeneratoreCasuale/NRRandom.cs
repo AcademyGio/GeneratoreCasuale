247f7bb8447e9bc697880c5fe5bcf439786a285c
Giorgio Vecchi <gvecchi@hotmail.com>
2021-03-16 15:39:27 +0100

    Add project files.

---
+++ GeneratoreCasuale/NRRandom.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GeneratoreCasuale
+{
+    class NRRandom
+    {
+        private int[] _numeri;
+
+        public int Rimasti { get; private set; }
+        private static void scambia(ref int a, ref int b)
+        {
+            int c = a;
+            a = b;
+            b = c;
+        }
+
+        // genera numeri compresi tra min incluso e max escluso
+        public NRRandom(int min, int max)
+        {
+
+        }
+
+        // costruisce un oggetto NRRandom che genera a caso n numeri diversi
+        // compresi tra 0 e n - 1
+        public NRRandom(int n)
+//            : this(0, n)    // chiamata di un costruttore da un costruttore
+        {
+            Rimasti = n;
+            _numeri = new int[n];
+            for (int i = 0; i < n; i++)
+                _numeri[i] = i;
+
+            Random r = new Random();
+
+            for (int i = 0; i < n - 1; i++)
+                // scambio l'iesimo con uno a caso dei successivi
+                scambia(ref _numeri[i], ref _numeri[r.Next(i + 1, n)]);
+        }
+
+        public int Next()   // questo potrebbe essere implementato come proprietà get
+        {
+            return _numeri[--Rimasti];
+        }
+    }
+}
---
+++ GeneratoreCasuale/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace GeneratoreCasuale
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            NRRandom nrr = new NRRandom(90);    // da 0 a 89
+            NRRandom nrr2 = new NRRandom(1, 91);    // da 1 a 90
+
+            while(nrr.Rimasti > 0)
+                Console.Write($"{nrr.Next()} ");
+
+            nnr.Reset();    // permette di ricominciare un intero di ciclo di generazioni
+
+        }
+    }
+}
