c9bc8c12b0b2a64b3fbbfc2beaaf8c9656f11be5
Giorgio Vecchi <gvecchi@hotmail.com>
2021-03-18 17:21:08 +0100

    Da min a max

--- GeneratoreCasuale/NRRandom.cs
+++ GeneratoreCasuale/NRRandom.cs
@@ -9,7 +9,8 @@ namespace GeneratoreCasuale
     class NRRandom
     {
         private int[] _numeri;
-
+        public int Min { get; }
+        public int Max { get; }
         public int Rimasti { get; private set; }
         private static void scambia(ref int a, ref int b)
         {
@@ -18,27 +19,38 @@ namespace GeneratoreCasuale
             b = c;
         }
 
+        // reimposta le condizioni iniziali
+        public void Reset()
+        {
+            Random r = new Random();
+            int n = Max - Min;
+
+            for (int i = 0; i < n - 1; i++)
+                // scambio l'iesimo con uno a caso dei successivi
+                scambia(ref _numeri[i], ref _numeri[r.Next(i + 1, n)]);
+
+            Rimasti = n;
+        }
+
         // genera numeri compresi tra min incluso e max escluso
         public NRRandom(int min, int max)
         {
+            Min = min;
+            Max = max;
+            int n = Max - Min;
+            _numeri = new int[n];
 
+            for (int i = 0; i < n; i++)
+                _numeri[i] = i + Min;
+
+            Reset();
         }
 
         // costruisce un oggetto NRRandom che genera a caso n numeri diversi
         // compresi tra 0 e n - 1
         public NRRandom(int n)
-//            : this(0, n)    // chiamata di un costruttore da un costruttore
+            : this(0, n)    // chiamata di un costruttore da un costruttore
         {
-            Rimasti = n;
-            _numeri = new int[n];
-            for (int i = 0; i < n; i++)
-                _numeri[i] = i;
-
-            Random r = new Random();
-
-            for (int i = 0; i < n - 1; i++)
-                // scambio l'iesimo con uno a caso dei successivi
-                scambia(ref _numeri[i], ref _numeri[r.Next(i + 1, n)]);
         }
 
         public int Next()   // questo potrebbe essere implementato come proprietà get
--- GeneratoreCasuale/Program.cs
+++ GeneratoreCasuale/Program.cs
@@ -6,14 +6,17 @@ namespace GeneratoreCasuale
     {
         static void Main(string[] args)
         {
-            NRRandom nrr = new NRRandom(90);    // da 0 a 89
-            NRRandom nrr2 = new NRRandom(1, 91);    // da 1 a 90
+            //NRRandom nrr = new NRRandom(90);    // da 0 a 89
+            NRRandom nrr = new NRRandom(1, 91);    // da 1 a 90
 
-            while(nrr.Rimasti > 0)
+            while (nrr.Rimasti > 0)
                 Console.Write($"{nrr.Next()} ");
 
-            nnr.Reset();    // permette di ricominciare un intero di ciclo di generazioni
+            nrr.Reset();    // permette di ricominciare un intero di ciclo di generazioni
 
+            Console.WriteLine();
+            while (nrr.Rimasti > 0)
+                Console.Write($"{nrr.Next()} ");
         }
     }
 }
